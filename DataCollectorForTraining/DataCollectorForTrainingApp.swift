429d671c19d78ebe900f01c4d9bb0d8242c5b286
Daniel Gurbin <daniel.g@msapps.mobi>
2024-11-29 15:21:34 +0200

    Initial Commit

---
+++ DataCollectorForTraining/DataCollectorForTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataCollectorForTrainingApp.swift
+//  DataCollectorForTraining
+//
+//  Created by Daniel Gurbin on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct DataCollectorForTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
